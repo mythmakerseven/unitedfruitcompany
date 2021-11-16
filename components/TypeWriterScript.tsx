ea1c4b87bdefe80b9f603ffe647ffb8f77f9ca88
Camden Mecklem <c@camdenmecklem.com>
2021-11-16 16:21:22 -0500

    use opacity modification in TypeWriterScript

--- components/TypeWriterScript.tsx
+++ components/TypeWriterScript.tsx
@@ -4,22 +4,41 @@ interface Props {
   text: string
 }
 
-const TypeWriterScript: React.FC<Props> = ({ text }) => {
-  const [displayedText, setDisplayedText] = useState<string>('')
+const getOpacity = (itemIndex: number, currentIndex: number) => {
+  if (itemIndex < currentIndex) {
+    return '1'
+  } else {
+    return '0'
+  }
+}
 
+const createSpans = (text: string, currentIndex: number) => {
+  return text.split('').map((char, index) => <span key={index} style={{ filter: `opacity(${getOpacity(index, currentIndex)})` }}>{char}</span>)
+}
+
+const TypeWriterScript: React.FC<Props> = ({ text }) => {  
+  
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [atomizedElements, setAtomizedElements] = useState(createSpans(text, currentIndex))
+  
+  
   useEffect(() => {
-    if (displayedText !== text) {
-      const timeout = Math.floor(Math.random() * 130)
-      const timedFunction = setTimeout(() => setDisplayedText(`${displayedText}${text[displayedText.length]}`), timeout)
+
+    if (currentIndex < text.length + 1) {
+      const timeout = Math.floor(Math.random() * 150)
+      const timedFunction = setTimeout(() => {
+        setCurrentIndex(currentIndex + 1)
+        setAtomizedElements(createSpans(text, currentIndex))
+      }, timeout)
 
       // Return a cleanup function that cancels the timeout if the component unmounts.
       return () => { clearTimeout(timedFunction) }
     }
-  }, [displayedText, text])
+  }, [atomizedElements, currentIndex, text])
 
   return (
     <>
-      { displayedText }
+      { atomizedElements }
     </>
   )
 }
