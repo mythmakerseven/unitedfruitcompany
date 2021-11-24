9a8ec6a37bce8ca24e554c3f366525839e06079b
Camden Mecklem <c@camdenmecklem.com>
2021-11-23 20:49:50 -0500

    add docs link to navbar

--- components/Navbar/NavLinks.tsx
+++ components/Navbar/NavLinks.tsx
@@ -5,6 +5,7 @@ const NavLinks: React.FC = () => {
     <>
       <Link href='/bios'>Bios</Link>
       <Link href='/articles'>Articles</Link>
+      <Link href='/documents'>Documents</Link>
       <Link href='/pictures'>Pictures</Link>
       <Link href='/timeline'>Timeline</Link>
       <Link href='/resources'>Resources</Link>
