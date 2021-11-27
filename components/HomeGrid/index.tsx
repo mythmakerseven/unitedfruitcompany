ad8ea48b69cae0ac383b7d2b5d6f89a18c76d156
Camden Mecklem <c@camdenmecklem.com>
2021-11-26 19:00:15 -0500

    request home grid pics at 480px width to match containers

--- components/HomeGrid/index.tsx
+++ components/HomeGrid/index.tsx
@@ -8,32 +8,32 @@ const HomeGrid: React.FC = () => {
         <HomeGridItem
           link='/bios'
           label='Biographies'
-          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/anne-nygard-h7-v2kkceci-unsplash.jpg?w=560'
+          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/anne-nygard-h7-v2kkceci-unsplash.jpg?w=480'
         />
         <HomeGridItem
           link='/articles'
           label='Articles'
-          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/laura-rivera-9zqzrlwv52m-unsplash.jpg?w=560'
+          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/laura-rivera-9zqzrlwv52m-unsplash.jpg?w=480'
         />
         <HomeGridItem
           link='/documents'
           label='Documents'
-          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/sear-greyson-k-zsc7ydj6y-unsplash.jpg?w=560'
+          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/sear-greyson-k-zsc7ydj6y-unsplash.jpg?w=480'
         />
         <HomeGridItem
           link='/pictures'
           label='Pictures'
-          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/roman-kraft-7spg5olfexc-unsplash.jpg?w=560'
+          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/roman-kraft-7spg5olfexc-unsplash.jpg?w=480'
         />
         <HomeGridItem
           link='/timeline'
           label='Timeline'
-          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/christina-isabella-pivmfs2l9b8-unsplash.jpg?w=560'
+          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/christina-isabella-pivmfs2l9b8-unsplash.jpg?w=480'
         />
         <HomeGridItem
           link='/resources'
           label='Resources'
-          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/jace-afsoon-5xmfg-eghpw-unsplash.jpg?w=560'
+          imagePath='https://unitedfruitcompany.files.wordpress.com/2021/11/jace-afsoon-5xmfg-eghpw-unsplash.jpg?w=480'
         />
       </FlexContainerContent>
     </FlexContainer>
