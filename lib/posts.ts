1ad86e4d27105a63634eec1872d8d18c09ff50f7
Camden Mecklem <c@camdenmecklem.com>
2021-11-27 23:40:48 -0500

    add support for paginated responses from WP

--- lib/posts.ts
+++ lib/posts.ts
@@ -22,6 +22,25 @@ const getCachedPosts = () => {
   }
 }
 
+const getPaginatedResponse = async (url: string, page = 1): Promise<Post[]> => {
+  console.log(`Getting page ${page}...`)
+
+  const response = await axios({
+    method: 'GET',
+    url: `${url}?page=${page}`
+  })
+
+  let posts = response.data.posts as Post[]
+
+  // Check if WP says there's another page
+  if (response.data.meta.next_page) {
+    const nextPage = await getPaginatedResponse(url, page + 1)
+    posts = posts.concat(nextPage)
+  }
+
+  return posts
+}
+
 const postRequest = async () => {
   if (!postsURL) {
     return [] as Post[]
@@ -36,27 +55,18 @@ const postRequest = async () => {
 
   console.log('Requesting posts from Wordpress API...')
 
-  let response
+  const posts = await getPaginatedResponse(postsURL)
 
-  try {
-    response = await axios({
-      method: 'GET',
-      url: postsURL
-    })
-  } catch {
-    response = { data: { posts: [] } }
-  }
-
-  const posts = (response.data.posts as Post[]).map(post => ({ ...post, title: decode(post.title) }))
+  const decodedPosts = (posts as Post[]).map(post => ({ ...post, title: decode(post.title) }))
 
   // Create the cache directory if it doesn't exist.
   if (!fs.existsSync(cacheFolder)) {
     fs.mkdirSync(cacheFolder)
   }
 
-  fs.writeFileSync(cacheFile, JSON.stringify(posts))
+  fs.writeFileSync(cacheFile, JSON.stringify(decodedPosts))
 
-  return posts
+  return decodedPosts
 }
 
 // Accepts an optional category argument that will only return matching posts.
