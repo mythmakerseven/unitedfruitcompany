d7716d0d2ffdc8c3147e3eb3ab2db1542ce93907
Camden Mecklem <c@camdenmecklem.com>
2021-12-09 16:16:50 -0500

    fix timeline images flashing during fast scrolling

--- components/PostTimeline/ImageBox.tsx
+++ components/PostTimeline/ImageBox.tsx
@@ -11,25 +11,40 @@ interface Props {
 
 const ImageBox: React.FC<Props> = ({ currentPost }) => {
   const [opacity, setOpacity] = useState(0)
-  const [imageURL, setImageURL] = useState(currentPost.featured_image)
+  const [currentImage, setCurrentImage] = useState(currentPost.featured_image)
 
-  const boxRef = useRef<HTMLDivElement>(null)
+  // Keep track of whether we're waiting for a timeout to finish.
+  // This prevents flashing images when the user quickly scrolls.
+  const timeoutRef = useRef<null | number>(null)
 
   useEffect(() => {
-    if (boxRef.current) {
-      setOpacity(0)
-      setTimeout(() => {
-        setImageURL(currentPost.featured_image)
+    const transition = () => {
+      if (currentPost.featured_image) {
+        setCurrentImage(currentPost.featured_image)
         setOpacity(1)
-      }, 400)
+      }
     }
-  }, [currentPost, imageURL])
+
+    // Clear any existing timeout
+    if (timeoutRef.current) {
+      console.log('clearing old timeout')
+      window.clearTimeout(timeoutRef.current)
+    }
+
+    setOpacity(0)
+    timeoutRef.current = window.setTimeout(() => transition(), 400)
+
+    return () => {
+      if (timeoutRef.current) {
+        window.clearTimeout(timeoutRef.current)
+      }
+    }
+  }, [currentPost])
 
   return (
     <Box
-      backgroundurl={`${imageURL}?w=800`}
+      backgroundurl={`${currentImage}?w=800`}
       opacity={opacity}
-      ref={boxRef}
     >
       <Link href={`/timeline/${currentPost.slug}`}>See more</Link>
     </Box>
