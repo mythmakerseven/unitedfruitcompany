5d5e8566fed3afd3fb118f4d7a892ba4689321b3
Camden Mecklem <c@camdenmecklem.com>
2021-12-09 20:22:00 -0500

    banana console easter egg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,27 @@ import Footer from '../components/Footer'
 import NavOffset from '../components/NavOffset'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (process.env.NODE_ENV === 'production') {
+    console.log(`
+            Hungry? Grab a banana!
+  
+   _
+  //\\
+  V  \\
+   \\  \\_
+    \\,'.\`-.
+     |\\ \`. \`.
+     ( \\  \`. \`-.                        _,.-:\\
+      \\ \\   \`.  \`-._             __..--' ,-';/
+       \\ \`.   \`-.   \`-..___..---'   _.--' ,'/
+        \`. \`.    \`-._        __..--'    ,' /
+          \`. \`-_     \`\`--..''       _.-' ,'
+            \`-_ \`-.___        __,--'   ,'
+               \`-.__  \`----"""    __.-'
+  hh                \`--..____..--'
+  
+    `)
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
