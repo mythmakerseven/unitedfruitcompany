516b71550f2c1a7b980a03bada1036d80e574ec0
Camden Mecklem <c@camdenmecklem.com>
2021-12-22 18:24:11 -0500

    update hero text

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,8 +58,8 @@ const Home: NextPage = () => {
       />
 
       <Hero
-        header='Gabriel García Márquez'
-        text='Many authors have mentioned the United Fruit Company in their books. Gabriel García Márquez (pictured), Thomas Pynchon, O. Henry, Pablo Neruda, and many more have been inspired by the history of the company as detailed on this site.'
+        header='In the Arts'
+        text='The United Fruit Company has inspired for numerous writers over the past hundred years. Gabriel García Márquez (pictured), Thomas Pynchon, O. Henry, Pablo Neruda, and many more have included references to the company in their stories.'
         picturePath='https://unitedfruitcompany.files.wordpress.com/2021/11/gabriel-garcia-marquez.jpg?w=600'
         pictureAltText='Portrait of Gabriel García Márquez'
         side={Side.Left}
