73bd6c5422948110c8d05479f0efe9a67944e25c
Camden Mecklem <c@camdenmecklem.com>
2022-11-05 16:27:15 -0400

    update personal site link

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from './styles'
 const Footer: React.FC = () => {
   return (
     <FooterContainer>
-      <p>Site created by <a href='https://camdenmecklem.com'>Camden Mecklem</a>.</p>
+      <p>Site created by <a href='https://camden.lol'>Camden Mecklem</a>.</p>
       <p>Content created by HIS394/HNR331, proctored by Dr. Jonathan T. Reynolds.</p>
       <AudioSecret
         child={<span>🍌</span>}
