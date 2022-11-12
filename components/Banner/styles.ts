74b2a883c3acc80b5d389d5d39ef3fe89dd7eb18
Camden Mecklem <c@camdenmecklem.com>
2022-11-12 15:45:24 -0500

    use p instead of h1 for banners

--- components/Banner/styles.ts
+++ components/Banner/styles.ts
@@ -18,7 +18,7 @@ export const ContainerContent = styled.div`
   padding: 0 20px;
 `
 
-export const Header = styled.h1`
+export const Header = styled.p`
   font-size: 2rem;
 
   @media (max-width: 800px) {
