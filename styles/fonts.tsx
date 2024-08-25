442b185e6fe63ee0fe9a65b26fd677d6d66bd424
Camden Mecklem <c@camdenmecklem.com>
2024-08-25 16:15:41 -0400

    replace @next/font with included next/font

--- styles/fonts.tsx
+++ styles/fonts.tsx
@@ -1,7 +1,13 @@
-import { Alegreya, Alegreya_Sans, Special_Elite } from '@next/font/google'
+import { Alegreya, Alegreya_Sans, Special_Elite } from "next/font/google";
 
-export const specialElite = Special_Elite({ subsets: ['latin'], weight: '400' })
+export const specialElite = Special_Elite({
+  subsets: ["latin"],
+  weight: "400",
+});
 
-export const alegreya = Alegreya({ subsets: ['latin'] })
+export const alegreya = Alegreya({ subsets: ["latin"] });
 
-export const alegreyaSans = Alegreya_Sans({ subsets: ['latin'], weight: '500' })
+export const alegreyaSans = Alegreya_Sans({
+  subsets: ["latin"],
+  weight: "500",
+});
